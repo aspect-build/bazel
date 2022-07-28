9621d3ff15dc531c68dd54fbab02d4859ed7dccc
Fabian Meumertzheim <fabian@meumertzhe.im>
2022-07-28 15:21:07 +0200

    Create and stage symlink artifacts with unmodified target path
    Unresolved symlink artifacts created with
    `ctx.actions.symlink(target_path = ...)` are now created without making
    the target path absolute by prepending the exec root, which diverged
    from the documentation and intended goal and also gave rise to
    hermeticity issues as such symlinks would regularly resolve outside the
    sandbox.
    
    Furthermore, in order to bring local execution in line with other
    execution types, the runfiles manifest entry (and thus the runfiles
    directory contents) for symlink artifacts are now their target paths
    rather than their exec paths, which along the way resolves another
    soure of non-hermetic resolution outside the runfiles directory.
    
    This requires making symlink artifacts (but not artifacts representing
    regular files) inputs of `SourceManifestAction`, which previously had no
    inputs. The change flattens a nested set in `getInputs`, but benchmarks
    confirm that this does not result in a performance regression.
    Alternatives considered either result in a substantially worse Starlark
    API or wouldn't work for symlinks created by spawns.

--- src/test/shell/bazel/remote/remote_execution_test.sh
+++ src/test/shell/bazel/remote/remote_execution_test.sh
@@ -3001,8 +3001,7 @@ EOF
     --spawn_strategy=remote \
     --remote_executor=grpc://localhost:${worker_port} \
     //pkg:b &>$TEST_log || fail "expected build to succeed"
-  # Using regex because the symlink is always absolute. https://github.com/bazelbuild/bazel/issues/14224
-  [[ $(cat bazel-bin/pkg/b.txt) =~ .*/non/existent ]] || fail "expected symlink target to be non/existent"
+  [[ $(cat bazel-bin/pkg/b.txt) == non/existent ]] || fail "expected symlink target to be non/existent"
 
   bazel clean --expunge
   bazel \
@@ -3013,8 +3012,7 @@ EOF
     --remote_executor=grpc://localhost:${worker_port} \
     --experimental_remote_merkle_tree_cache \
     //pkg:b &>$TEST_log || fail "expected build to succeed with Merkle tree cache"
-  # Using regex because the symlink is always absolute. https://github.com/bazelbuild/bazel/issues/14224
-  [[ $(cat bazel-bin/pkg/b.txt) =~ .*/non/existent ]] || fail "expected symlink target to be non/existent"
+  [[ $(cat bazel-bin/pkg/b.txt) == non/existent ]] || fail "expected symlink target to be non/existent"
 }
 
 run_suite "Remote execution and remote cache tests"
